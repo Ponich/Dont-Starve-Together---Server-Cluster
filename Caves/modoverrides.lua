323eb94ed2f98470475e75bbf50ac4731d203fde
Nikolay Ponich <n.ponich@gmail.com>
2018-05-16 11:17:21 +0300

    mod enable

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -1,3 +1,90 @@
 return {
-  --["workshop-503919639"]={ configuration_options={ FIX_EQUIP_NOISE=false }, enabled=true } 
+  ["workshop-1079538195"]={
+    configuration_options={
+      beebox=true,
+      birdcage=true,
+      cartographydesk=true,
+      cookpot=true,
+      dragonflychest=true,
+      dragonflyfurnace=true,
+      endtable=true,
+      firesuppressor=true,
+      icebox=true,
+      lightning_rod=true,
+      meatrack=true,
+      modsupport=false,
+      moondial=true,
+      mushroom_farm=true,
+      mushroom_light=true,
+      nightlight=true,
+      perdshrine=true,
+      pottedfern=true,
+      rainometer=true,
+      researchlab=true,
+      researchlab2=true,
+      researchlab3=true,
+      researchlab4=true,
+      resurrectionstatue=true,
+      saltlick=true,
+      scarecrow=true,
+      sculptingtable=true,
+      succulent_potted=true,
+      townportal=true,
+      treasurechest=true,
+      wardrobe=true,
+      winterometer=true 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="RU", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-356420397"]={ configuration_options={  }, enabled=true },
+  ["workshop-361994110"]={
+    configuration_options={ HAT_DECAY=false, RELEASE_BEES=false, SPRING_BEES=true },
+    enabled=true 
+  },
+  ["workshop-369083494"]={
+    configuration_options={ AutoCrumble=true, Difficulty=0, Edible=true, GrowthSpeed=1 },
+    enabled=true 
+  },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
+  ["workshop-503919639"]={ configuration_options={ FIX_EQUIP_NOISE=true }, enabled=true },
+  ["workshop-755028761"]={ configuration_options={ Charcoal=5, Cutstones=5, Goldnugget=5 }, enabled=true },
+  ["workshop-758532836"]={
+    configuration_options={
+      AUTOPAUSECONSOLE=false,
+      AUTOPAUSEMAP=false,
+      AUTOPAUSESINGLEPLAYERONLY=true,
+      ENABLECLIENTPAUSE=false,
+      ENABLEHOTKEY=false,
+      KEYBOARDTOGGLEKEY="P" 
+    },
+    enabled=true 
+  } 
 }
